6195e9aca5eb43d0ac5f7c2f4d4d8f2d18fdc231
Luke9248 <67927220+Luke9248@users.noreply.github.com>
2023-05-18 09:20:47 -0400

    Update generateDeleteQuery.ts

--- tests/generateDeleteQuery.ts
+++ tests/generateDeleteQuery.ts
@@ -3,37 +3,35 @@ import {expect} from "chai";
 
 export function generateDeleteQuery(repository: any) {
   return async function () {
-    let PhoneNumber: GraphDBModelConstructor, Organization: GraphDBModelConstructor;
+    let PhoneNumber: GraphDBModelConstructor, Organization: GraphDBModelConstructor, Group: GraphDBModelConstructor;
     it('should create models', function () {
-      const Group = createGraphDBModel({
-        label: {type: String, internalKey: 'rdfs:label'},
-        comment: {type: String, internalKey: 'rdfs:comment'},
-        administrator: {type: GDBUserAccountModel, internalKey: ':hasAdministrator'},
-        organizations: {type: [GDBOrganizationModel], internalKey: ':hasOrganization'},
-      }, {
-        rdfTypes: [':Group'], name: 'group'
-      });
-
       const Organization = createGraphDBModel({
         comment: {type: String, internalKey: 'rdfs:comment'},
       }, {
         rdfTypes: ['cids:Organization'], name: 'organization'
       });
 
-     
+      const Group = createGraphDBModel({
+        label: {type: String, internalKey: 'rdfs:label'},
+        comment: {type: String, internalKey: 'rdfs:comment'},
+        organizations: {type: [Organization], internalKey: ':hasOrganization'},
+      }, {
+        rdfTypes: [':Group'], name: 'group'
+      });
+    })
 
-    it('should not create duplicated nested instance', async function () {
+
+
+    it('should not remove an organization from group', async function () {
       const group = Group({
-        label: 'group1'
+        label: 'group1',
         organizations: [{comment: 'org1'}]
       });
       await group.save();
       group.organizations = [];
       await group.save();
-<!--       expect(await UserAccount.find({})).length(1);
-      expect(await Person.find({})).length(1);
-      expect(await Address.find({})).length(2);
-      expect(await PhoneNumber.find({})).length(1); -->
+      expect(await group.find({})).to.have.property('organizations').length(0);
     });
   }
 }
+
